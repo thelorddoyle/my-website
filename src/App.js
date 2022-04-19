8460bc8d299033a3817147996f56e8789da103b8
Daniel Lord-Doyle <dlorddoyle@gmail.com>
2022-04-19 14:21:53 +1000

    added react transition group and some styling

--- src/App.js
+++ src/App.js
@@ -7,26 +7,38 @@ import { Footer } from './components/Footer'
 import { PreCoding } from './components/PreCoding'
 import { PostCoding } from './components/PostCoding';
 
+import {TransitionGroup, CSSTransition} from 'react-transition-group'
+
 import {
     BrowserRouter as Router,
     Routes,
     Route,
-    Link
+    useLocation
   } from "react-router-dom";
 
 function App() {
+
+    const location = useLocation()
+
   return (
     <div className="App">
-    <Router>
         <NavBar />
-        <Routes>
-            <Route path="/" element={<HomePage/>}/>
-            <Route path="about-me" element={<AboutMe/>} />
-            <Route path="/about-me/before" element={<PreCoding />} />
-            <Route path="/about-me/now" element={<PostCoding />} />
-        </Routes>
+        <TransitionGroup>
+            <CSSTransition
+                timeout={500}
+                classNames='fade'
+                key={location.key}>
+
+                <Routes location={location}>
+                    <Route path="/" element={<HomePage/>}/>
+                    <Route path="about-me" element={<AboutMe/>} />
+                    <Route path="/about-me/before" element={<PreCoding />} />
+                    <Route path="/about-me/now" element={<PostCoding />} />
+                </Routes>
+
+            </CSSTransition>
+        </TransitionGroup>
         <Footer />
-    </Router>
     </div>
   );
 }
